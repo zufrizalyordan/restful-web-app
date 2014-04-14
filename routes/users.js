a4d49a9b50df4e322bcab861e0dd1097d0c2264d
zufrizalyordan <zufrizalyordan@gmail.com>
2014-04-14 18:37:45 +0700

    route for adding user

--- routes/users.js
+++ routes/users.js
@@ -8,9 +8,19 @@ var db = mongo.db("mongodb://localhost:27017/rwa", {native_parser:true});
 /* GET users listing. */
 router.get('/', function(req, res) {
 
-    db.collection('userlist').find().toArray(function (err, items) {
-      res.json(items);
-    })
+  db.collection('userlist').find().toArray(function (err, items) {
+    res.json(items);
+  })
+
+});
+
+router.post('/', function(req, res){
+
+  db.collection('userlist').insert(req.body, function(err, result){
+    res.send(
+      (err === null) ? { msg: '' } : { msg: err }
+    );
+  });
 
 });
 
