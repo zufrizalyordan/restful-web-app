c826088097f956a71933a2e6042d005a5d820d04
zufrizalyordan <zufrizalyordan@gmail.com>
2014-04-14 18:11:38 +0700

    use the mongodb connection with the rwa db name

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var users = require('./routes/users');
 
 var app = express();
 
+// database access
+var mongo = require('mongoskin');
+var db = mongo.db("mongodb://localhost:27017/rwa", {native_parser:true});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
