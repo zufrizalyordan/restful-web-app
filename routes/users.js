58d18cbd50d4bb1002b156f8d5c3118395125fbc
zufrizalyordan <zufrizalyordan@gmail.com>
2014-04-14 18:12:50 +0700

    call mongodb connection in userlist route

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+// database access
+var mongo = require('mongoskin');
+var db = mongo.db("mongodb://localhost:27017/rwa", {native_parser:true});
+
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.send('respond with a resource');
